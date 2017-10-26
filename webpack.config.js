df57eec61e7fa6a54e55c63584a5de79c609a408
Normal <normaltic@naver.com>
2017-10-26 04:27:34 +0000

    webpack configs

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+    entry: [
+		'./src/index.js',
+		'webpack-dev-server/client?http://0.0.0.0:4001',
+	],
+
+    output: {
+        path: __dirname + '/public/',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+		inline: true,
+		hot: true,
+		host: "0.0.0.0",
+        port: 3001,
+		proxy: {
+			"**": "http://0.0.0.0:4000"
+		},
+        contentBase: __dirname + '/public/',
+		historyApiFallback: true,
+		disableHostCheck: true
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /mode_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    }
+};
